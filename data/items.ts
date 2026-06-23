86ce0f11e349143313508b60463fcab5219f06d5
Nubles <alexanderhaynes18@gmail.com>
2026-06-23 15:09:45 +0100

    Update items.ts with final RS3 details

--- data/items.ts
+++ data/items.ts
@@ -1,5 +1,3 @@
-
-
 export const SKILLS_LIST = [
   'Attack', 'Hitpoints', 'Mining', 
   'Strength', 'Agility', 'Smithing', 
@@ -8,21 +6,22 @@ export const SKILLS_LIST = [
   'Prayer', 'Crafting', 'Firemaking', 
   'Magic', 'Fletching', 'Woodcutting', 
   'Runecraft', 'Slayer', 'Farming', 
-  'Construction', 'Hunter', 'Summoning', 'Dungeoneering', 'Divination', 'Invention', 'Archaeology', 'Necromancy'
+  'Construction', 'Hunter', 'Summoning',
+  'Dungeoneering', 'Divination', 'Invention',
+  'Archaeology', 'Necromancy'
 ];
 
 export const EQUIPMENT_SLOTS = [
-  'Head', 'Cape', 'Neck', 'Ammo', 'Weapon', 'Body', 'Shield', 'Legs', 'Gloves', 'Boots', 'Ring'
+  'Head', 'Cape', 'Neck', 'Ammo', 'Weapon', 'Body', 'Shield', 'Legs', 'Gloves', 'Boots', 'Ring', 'Pocket', 'Aura'
 ];
 
 export const MOBILITY_LIST = [
-  'Spirit Trees', 'Fairy Rings', 'Gnome Gliders', 'Charter Ships', 'Teleport Tablets', 
-  'Jewelry Teleports', 'Canoes', 'Balloon Transport', 'Mine Carts', 'Magic Carpets', 
-  'Wilderness Obelisks', 'Minigame Teleports', 'Quetzal Network',
-  'Mycelium Transport', 'Eagle Transport', 'Slayer Ring', 'Xeric\'s Talisman',
-  'Drakan\'s Medallion', 'Pharaoh\'s Sceptre', 'Crystal Teleport Seed', 'Royal Seed Pod',
-  'Ectophial', 'Kharedst\'s Memoirs', 'Enchanted Lyre', 'Ring of the Elements',
-  'Digsite Pendant', 'Camulet'
+  'Lodestone Network', 'Fairy Rings', 'Spirit Trees', 'Gnome Gliders', 'Charter Ships', 
+  'Passage of the Abyss', 'Jewelry Teleports', 'Canoes', 'Balloon Transport', 'Mine Carts', 
+  'Wilderness Obelisks', 'Minigame Teleports', 'Dungeoneering Ring', 'Quetzal Network',
+  'Slayer Ring', 'Ectophial', 'Crystal Teleport Seed', 'Royal Seed Pod',
+  'Ring of Fortune', 'Passage Teleport', 'Enchanted Lyre', 'Ring of the Elements',
+  'Digsite Pendant', 'Games Necklace', 'Ring of Dueling', 'Combat Bracelet', 'Mycelium Transport', 'Eagle Transport', 'Xeric\'s Talisman', 'Drakan\'s Medallion', 'Pharaoh\'s Sceptre', 'Magic Carpets', 'Camulet', 'Kharedst\'s Memoirs'
 ];
 
 export const ARCANA_LIST = [
@@ -55,67 +54,138 @@ export const MERCHANTS_LIST = [
   'Slayer Equipment', 'Stonemasons', 'Sawmill Operators', 'Ore Merchants',
   'Real Estate Agents', 'Tanners', 'Taxidermists', 'Decanters', 'Lost Property',
   'Warhammer Shops', 'Claw Shops', 'Halberd Shops', 'Pet Shops',
-  'Weapon Shops', 'Reward Shops'
+  'Weapon Shops', 'Reward Shops', 'Invention Components', 'Archaeology Materials',
+  'Necromancy Shrouds'
 ];
 
 export const STORAGE_LIST = [
-  'Looting Bag', 'Rune Pouch', 'Seed Box', 'Herb Sack', 'Gem Bag', 'Coal Bag', 'Fish Barrel', 
+  'Ore Box', 'Material Storage', 'Coin Pouch', 'Rune Pouch', 'Seed Box', 'Herb Sack', 'Gem Bag', 'Coal Bag', 'Fish Barrel', 
   'Tackle Box', 'Bolt Pouch', 'Plank Sack', 'Huntsman\'s Kit', 'Log Basket', 'Beginner STASH', 
   'Easy STASH', 'Medium STASH', 'Hard STASH', 'Elite STASH', 'Master STASH', 'Tool Leprechauns',
   'Meat Pouch', 'Essence Pouches', 'Master Scroll Book', 'Steel Key Ring', 'Bottomless Bucket',
-  'Spice Pouch', 'Flamtaer Bag', 'Seed Vault', 'Fossil Storage',
-  'Colossal Pouch', 'Dizana\'s Quiver', 'Forestry Kit', 'Gricoller\'s Can'
+  'Spice Pouch', 'Seed Vault', 'Fossil Storage', 'Colossal Pouch', 'Gricoller\'s Can', 'Dizana\'s Quiver', 'Forestry Kit', 'Flamtaer Bag'
 ];
 
 export const GUILDS_LIST = [
   'Champions\' Guild', 'Cooks\' Guild', 'Crafting Guild', 'Mining Guild', 'Prayer Guild', 
   'Farming Guild', 'Fishing Guild', 'Heroes\' Guild', 'Hunter Guild', 'Legends\' Guild',
-  'Myths\' Guild', 'Ranging Guild', 'Rogues\' Den', 'Servants\' Guild', 'Warriors\' Guild',
-  'Wizards\' Guild', 'Woodcutting Guild'
+  'Myths\' Guild', 'Ranging Guild', 'Warriors\' Guild', 'Wizards\' Guild', 'Woodcutting Guild',
+  'Invention Guild', 'Archaeology Guild', 'Servants\' Guild'
 ];
 
-// Slayer reward-point "Unlocks" (verified against the OSRS wiki Slayer Rewards
-// interface): monster-assignment unlocks, the finishing-blow toggles, and the
-// crafting/utility unlocks bought with Slayer points.
 export const SLAYER_UNLOCKS_LIST = [
-  'Malevolent Masquerade', 'Broader Fletching', 'Ring Bling', 'Bigger and Badder',
+  'Slayer Helmet', 'Broad Arrow Fletching', 'Ring Bling', 'Bigger and Badder',
   'Like a Boss', 'Task Storage', 'Gargoyle Smasher', 'Slug Salter', 'Reptile Freezer',
-  "'Shroom Sprayer", 'Duly Noted', 'Stop the Wyvern', 'Double Trouble',
-  'Seeing Red', 'Watch the Birdie', 'Hot Stuff', 'Reptile Got Ripped', 'Basilocked',
-  'Actual Vampyre Slayer', 'I Wildy More Slayer', 'Warped Reality', 'Lured In',
-  'Wings Spread'
+  'Reaper Tasks', 'Ability Codex Unlocks'
 ];
 
 export const FARMING_PATCH_LIST = [
   'Allotment', 'Herb', 'Flower', 'Hops', 'Bush', 'Wood Tree', 
-  'Fruit Tree', 'Hardwood Tree', 'Cactus', 'Mushroom', 'Belladonna', 
-  'Seaweed', 'Calquat', 'Spirit Tree', 'Celastrus', 'Redwood', 
-  'Crystal Tree', 'Hespori Patch', 'Anima', 'Vinery', 'Coral Nursery'
+  'Fruit Tree', 'Hardwood Tree', 'Cactus', 'Mushroom', 'Spirit Tree', 'Elder Tree', 'Belladonna', 'Seaweed', 'Calquat', 'Celastrus', 'Redwood', 'Crystal Tree', 'Hespori Patch', 'Anima', 'Vinery', 'Coral Nursery'
 ];
 
 export const FARMING_UNLOCK_DETAILS: Record<string, string> = {
-  'Allotment': "Potatoes, Watermelons, Snape Grass",
-  'Herb': "Guam, Ranarr, Snapdragon, Torstol",
-  'Flower': "Marigold, Limpwurt, White Lily",
-  'Hops': "Barley, Jute, Yanillian",
-  'Bush': "Redberry, Whiteberry, Poison Ivy",
-  'Wood Tree': "Oak, Yew, Magic",
-  'Fruit Tree': "Apple, Palm, Dragonfruit",
+  'Allotment': "Potatoes, Cabbages, Watermelons",
+  'Herb': "Guam, Ranarr, Torstol, Fellstalk",
+  'Flower': "Marigold, Nasturtium, Woad",
+  'Hops': "Barley, Krandorian, Wildblood",
+  'Bush': "Redberry, Poison Ivy, Wishing Well Bush",
+  'Wood Tree': "Oak, Willow, Maple, Yew, Magic",
+  'Fruit Tree': "Apple, Banana, Orange, Palm",
   'Hardwood Tree': "Teak, Mahogany",
   'Cactus': "Cactus Spine, Potato Cactus",
-  'Mushroom': "Bittercap",
-  'Belladonna': "Nightshade",
-  'Seaweed': "Giant Seaweed",
-  'Calquat': "Calquat Fruit",
+  'Mushroom': "Bittercap, Spirit Mushroom",
   'Spirit Tree': "Teleport Network",
-  'Celastrus': "Celastrus Bark",
-  'Redwood': "Redwood Logs",
-  'Crystal Tree': "Crystal Shards",
-  'Hespori Patch': "Bottomless Bucket, Anima Seeds",
-  'Anima': "Attas, Iasor, Kronos",
-  'Vinery': "Grapes (for wine)",
-  'Coral Nursery': "Coral (potion ingredients)"
+  'Elder Tree': "Elder Logs"
 };
+
+export const MISTHALIN_AREAS = [
+  'Varrock', 'Lumbridge', 'Draynor Village', 'Wizards\' Tower', 'Edgeville', 
+  'Barbarian Village', 'Digsite', 'Silvarea', 'Paterdomus'
+];
+
+export const REGION_GROUPS: Record<string, string[]> = {
+  'Asgarnia': [
+    'Falador', 'Port Sarim', 'Rimmington', 'Taverley', 'Burthorpe', 'Warriors\' Guild', 
+    'Heroes\' Guild', 'Crafting Guild', 'Dwarven Mine', 'Ice Mountain', 'Asgarnian Ice Dungeon', 
+    'Motherlode Mine', 'Goblin Village', 'Mudskipper Point', 'Void Knights\' Outpost', 'Entrana'
+  ],
+  'Kandarin': [
+    'East Ardougne', 'West Ardougne', 'Catherby', 'Seers\' Village', 'Camelot', 'Yanille', 
+    'Port Khazard', 'Hemenster', 'Fishing Guild', 'Ranging Guild', 'Legends\' Guild', 
+    'Tree Gnome Stronghold', 'Gnome Village', 'Witchaven', 'Piscatoris Fishing Colony', 
+    'Feldip Hills', 'Baxtorian Falls', 'Otto\'s Grotto', 'Barbarian Outpost', 'Fight Arena',
+    'Castle Wars', 'Corsair Cove', 'Eagles\' Peak', 'Observatory', 'Ourania Altar'
+  ],
+  'Karamja': [
+    'Musa Point', 'Brimhaven', 'Tai Bwo Wannai', 'Shilo Village', 'Kharazi Jungle', 
+    'Mor Ul Rek (TzHaar City)', 'Crandor', 'Ship Yard'
+  ],
+  'Kharidian Desert': [
+    'Al Kharid', 'Duel Arena / PvP Arena', 'Shantay Pass', 'Pollnivneach', 'Nardah', 
+    'Sophanem', 'Menaphos', 'Bandit Camp', 'Bedabin Camp', 'Ruins of Uzer', 
+    'Mage Training Arena', 'Agility Pyramid', 'Giants\' Plateau', 'Kalphite Lair',
+    'Ruins of Unkah'
+  ],
+  'Morytania': [
+    'Canifis', 'Port Phasmatys', 'Mort\'ton', 'Barrows', 'Burgh de Rott', 'Meiyerditch', 
+    'Darkmeyer', 'Slepe', 'Ver Sinhaza', 'Fenkenstrain\'s Castle', 'Slayer Tower', 
+    'Mort Myre Swamp', 'Haunted Mine', 'Haunted Woods', 'Harmony Island', 
+    'Mos Le\'Harmless', 'Braindeath Island', 'Dragontooth Island', 'Icyene Graveyard'
+  ],
+  'Fremennik': [
+    'Rellekka', 'Neitiznot', 'Jatizso', 'Miscellania & Etceteria', 'Waterbirth Island', 
+    'Lunar Isle', 'Mountain Camp', 'Lighthouse', 'Keldagrim', 'Weiss', 'Pirates\' Cove'
+  ],
+  'Tirannwn': [
+    'Prifddinas', 'Lletya', 'Tyras Camp', 'Elf Camp', 'Isafdar', 'Zul-Andra', 
+    'Arandar', 'Gwenith', 'Iorwerth Camp', 'Poison Waste'
+  ],
+  'Wilderness': [
+    'Ferox Enclave', 'Wilderness Volcano', 'Chaos Temple', 'Rogues\' Castle', 'Lava Maze', 
+    'Wilderness Bandit Camp', 'Dark Warriors\' Fortress', 'Graveyard of Shadows', 'Forgotten Cemetery',
+    'Resource Area', 'Mage Arena', 'Scorpia\'s Cave', 'Fountain of Rune', 'Wilderness God Wars Dungeon',
+    'Daimon\'s Crater'
+  ],
+  'Daemonheim': [
+    'Daemonheim Peninsula', 'Frozen Floors', 'Abandoned Floors', 'Furnished Floors', 'Occult Floors', 'Warped Floors', 'Daemonheim Castle', 'Catacombs of Daemonheim'
+  ],
+  'Anachronia': [
+    'Anachronia Base Camp', 'Lazul Outpost', 'Archaeology Digsite (Anachronia)', 'Agility Course (Anachronia)', 'Slayer Dungeon (Anachronia)'
+  ],
+  'City of Um': [
+    'Underworld', 'Ritual Site', 'Um Docks', 'Necryo Tomb', 'Soul Well'
+  ],
+  'Eastern Isles': [
+    'Aminishi', 'Tuai Leit', 'Gielinor\'s Edge', 'Waiko', 'Whale\'s Teeth', 'Turtle Islands'
+  ],
+  'Senntisten': [
+    'Cathedral', 'Asylum', 'Senntisten Graveyard', 'Zarosian Crypts' ]
+  ,
+  // --- Legacy OSRS Regions (for validation compatibility) ---
+  'Kourend & Kebos': [
+    'Kourend Castle', 'Hosidius', 'Piscarilius', 'Shayzien', 'Lovakengj', 'Arceuus', 
+    'Kebos Lowlands', 'Molch', 'Farming Guild', 'Woodcutting Guild', 'Mount Quidamortem', 
+    'Mount Karuulm', 'Catacombs of Kourend', 'Land\'s End', 'Wintertodt Camp'
+  ],
+  'Varlamore': [
+    'Civitas illa Fortis', 'Avium Savannah', 'Cam Torum', 'Ralos\' Rise', 'Darkfrost', 
+    'Hunter\'s Guild', 'Aldarin', 'The Stranglewood', 'Tlati Rainforest', 'Auburnvale', 'Mistrock', 'Sunset Coast'
+  ],
+  'Islands & Others': [
+    'Fossil Island', 'Ape Atoll', 'Zanaris', 'Tutorial Island', 'Isle of Souls', 'Lithkren'
+  ],
+  'The Open Seas': [
+    'The Pandemonium', 'Dognose Island', 'Chinchompa Island', 'Remote Island', 'The Great Conch',
+    'The Little Pearl', 'Vatrachos Island', 'The Onyx Crest', 'Shimmering Atoll', 'Port Roberts',
+    'Anglers\' Retreat', 'Minotaurs\' Rest', 'Isle of Bones', 'Laguna Aurorae', 'Charred Island',
+    'Tear of the Soul', 'Wintumber Island', 'The Crown Jewel', 'Lledrith Island', 'Deepfin Point',
+    'Rainbow\'s End', 'Sunbleak Island', 'Ynysdail', 'Buccaneers\' Haven', 'Drumstick Isle',
+    'Brittle Isle', 'Grimstone', 'Red Rock', 'Last Light', 'The Summer Shore'
+  ]
+};;
+
+export const REGIONS_LIST = Object.values(REGION_GROUPS).flat();
 
 export const BOSSES_LIST = [
   'Temple of Aminishi', 'Dragonkin Laboratory', 'Shadow Reef', 'Zamorakian Underworld', 'Arch-Glacor', 'Telos', 'Raksha', 
@@ -145,82 +215,3 @@ export const MINIGAMES_LIST = [
   'Warriors\' Guild', 'Burthorpe Games Room', 'Forestry', 'Rat Pits', 'Tears of Guthix',
   'Brimhaven Agility Arena', 'Hallowed Sepulchre', 'Barracuda Trials'
 ];
-
-export const MISTHALIN_AREAS = [
-  'Varrock', 'Lumbridge', 'Draynor Village', 'Wizards\' Tower', 'Edgeville', 
-  'Barbarian Village', 'Digsite', 'Silvarea', 'Paterdomus'
-];
-
-export const REGION_GROUPS: Record<string, string[]> = {
-  'Asgarnia': [
-    'Falador', 'Port Sarim', 'Rimmington', 'Taverley', 'Burthorpe', 'Warriors\' Guild', 
-    'Heroes\' Guild', 'Crafting Guild', 'Dwarven Mine', 'Ice Mountain', 'Asgarnian Ice Dungeon', 
-    'Motherlode Mine', 'Goblin Village', 'Mudskipper Point', 'Void Knights\' Outpost', 'Entrana'
-  ],
-  'Kandarin': [
-    'East Ardougne', 'West Ardougne', 'Catherby', 'Seers\' Village', 'Camelot', 'Yanille', 
-    'Port Khazard', 'Hemenster', 'Fishing Guild', 'Ranging Guild', 'Legends\' Guild', 
-    'Tree Gnome Stronghold', 'Gnome Village', 'Witchaven', 'Piscatoris Fishing Colony', 
-    'Feldip Hills', 'Baxtorian Falls', 'Otto\'s Grotto', 'Barbarian Outpost', 'Fight Arena'
-  , 'Castle Wars', 'Corsair Cove', 'Eagles\' Peak', 'Observatory', 'Ourania Altar'
-  ],
-  'Karamja': [
-    'Musa Point', 'Brimhaven', 'Tai Bwo Wannai', 'Shilo Village', 'Kharazi Jungle', 
-    'Mor Ul Rek (TzHaar City)', 'Crandor'
-  , 'Ship Yard'
-  ],
-  'Kharidian Desert': [
-    'Al Kharid', 'Duel Arena / PvP Arena', 'Shantay Pass', 'Pollnivneach', 'Nardah', 
-    'Sophanem', 'Menaphos', 'Bandit Camp', 'Bedabin Camp', 'Ruins of Uzer', 
-    'Mage Training Arena', 'Agility Pyramid', 'Giants\' Plateau', 'Kalphite Lair'
-  , 'Ruins of Unkah'
-  ],
-  'Morytania': [
-    'Canifis', 'Port Phasmatys', 'Mort\'ton', 'Barrows', 'Burgh de Rott', 'Meiyerditch', 
-    'Darkmeyer', 'Slepe', 'Ver Sinhaza', 'Fenkenstrain\'s Castle', 'Slayer Tower', 
-    'Mort Myre Swamp', 'Haunted Mine', 'Haunted Woods', 'Harmony Island', 
-    'Mos Le\'Harmless', 'Braindeath Island', 'Dragontooth Island'
-  , 'Icyene Graveyard'
-  ],
-  'Fremennik': [
-    'Rellekka', 'Neitiznot', 'Jatizso', 'Miscellania & Etceteria', 'Waterbirth Island', 
-    'Lunar Isle', 'Mountain Camp', 'Lighthouse', 'Keldagrim'
-  , 'Weiss', 'Pirates\' Cove'
-  ],
-  'Tirannwn': [
-    'Prifddinas', 'Lletya', 'Tyras Camp', 'Elf Camp', 'Isafdar', 'Zul-Andra', 
-    'Arandar', 'Gwenith', 'Iorwerth Camp'
-  , 'Poison Waste'
-  ],
-  'Wilderness': [
-    'Ferox Enclave', 'Wilderness Volcano', 'Chaos Temple', 'Rogues\' Castle', 'Lava Maze', 
-    'Wilderness Bandit Camp', 'Dark Warriors\' Fortress', 'Graveyard of Shadows', 'Forgotten Cemetery',
-    'Resource Area', 'Mage Arena', 'Scorpia\'s Cave', 'Fountain of Rune', 'Wilderness God Wars Dungeon'
-  , 'Daimon\'s Crater'
-  ],
-  'Kourend & Kebos': [
-    'Kourend Castle', 'Hosidius', 'Piscarilius', 'Shayzien', 'Lovakengj', 'Arceuus', 
-    'Kebos Lowlands', 'Molch', 'Farming Guild', 'Woodcutting Guild', 'Mount Quidamortem', 
-    'Mount Karuulm', 'Catacombs of Kourend', 'Land\'s End', 'Wintertodt Camp'
-  ],
-  'Varlamore': [
-    'Civitas illa Fortis', 'Avium Savannah', 'Cam Torum', 'Ralos\' Rise', 'Darkfrost', 
-    'Hunter\'s Guild', 'Aldarin', 'The Stranglewood'
-  , 'Tlati Rainforest', 'Auburnvale', 'Mistrock', 'Sunset Coast'
-  ],
-  'Islands & Others': [
-    'Fossil Island', 'Ape Atoll', 'Zanaris', 'Tutorial Island'
-  , 'Isle of Souls', 'Lithkren'
-  ],
-  'The Open Seas': [
-    'The Pandemonium', 'Dognose Island', 'Chinchompa Island', 'Remote Island', 'The Great Conch',
-    'The Little Pearl', 'Vatrachos Island', 'The Onyx Crest', 'Shimmering Atoll', 'Port Roberts',
-    'Anglers\' Retreat', 'Minotaurs\' Rest', 'Isle of Bones', 'Laguna Aurorae', 'Charred Island',
-    'Tear of the Soul', 'Wintumber Island', 'The Crown Jewel', 'Lledrith Island', 'Deepfin Point',
-    'Rainbow\'s End', 'Sunbleak Island', 'Ynysdail', 'Buccaneers\' Haven', 'Drumstick Isle',
-    'Brittle Isle', 'Grimstone'
-  , 'Red Rock', 'Last Light', 'The Summer Shore'
-  ]
-};
-
-export const REGIONS_LIST = Object.values(REGION_GROUPS).flat();
